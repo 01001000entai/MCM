a54c66e7f49aca2e71fb489608dee37fd8b058bf
01001000entai <01001000entai@gmail.com>
2015-02-09 01:27:48 +0800

    v0.2

--- 2015ICM/Sim2.py
+++ 2015ICM/Sim2.py
@@ -5,7 +5,7 @@ class CT():
         self.num = 0
         self.PN = [6,30,52,66,71,79,90] #wait
         self.CA = [6,30,52,66,71,79,90]
-        self.EXP = [0,0,1,1,2,2,3]
+        self.EXP = [0,0,12,12,24,24,36]
         self.NEW_EXP = [2,1,3,4,5,6,7]
     def new_id(self):
         ret = self.tot
@@ -25,12 +25,13 @@ class Worker():
         self.CA = ct.check(random.guass(50,20))
         self.EXP = random.randint(0,10)
         self.name_of_department = ""
-        self.id = ct.new_id
+        self.id = ct.new_id()
         self.level = lv
+        self.sum_leave = 0
         if new == True:
             self.EXP = -ct.NEW_EXP[self.level]
-    def cal_proformance():
-        return math.sqrt((self.PN-ct.PN[self.level])**2 + (self.CA-ct.CA[self.level])**2
+    def cal_proformance(self):
+        return 0.25 * self.CA + 0.25 * self.EX + 0.5 * self.PN
 
 class Department():
     def __init__(self, name_of_department, list_of_worker):
@@ -50,6 +51,22 @@ class Human_Capital_Network():
 
     def add_department(self, name_of_department, list_of_worker):
         self.department.append(Department(name_of_department, list_of_worker))
+
+    def cal_alp(self, k):
+        ret = ct.alp[self.level_of_position[k]] * (1-self.position[k].PN/100)
+        l = len(self.position)
+        s1 = 0
+        s2 = self.position[k]
+        for i in range(l):
+            if self.position[i] != None:
+                if self.mat[k][i] == 1:
+                    if self.position[i].EXP < 0:
+                        s1 += self.position[i].PN
+                    else:
+                        s2 += self.position[i].PN
+        ret += s1/s2
+
+
 
     def update_position(self):
         self.position = list()
@@ -77,8 +94,7 @@ class Human_Capital_Network():
         l = len(self.position)
         for i in range(l):
             if self.position[i] != None and self.mat[k][i] == 1:
-                self.position[i].PN = ct.check(self.position[i].PN - self.position[k].PN*self.level_of_position[k]/7)
-
+                self.position[i].sum_leave += self.position[k]
 
     def invite(self, k):
         if self.level_of_position[k] <= 1:
@@ -103,6 +119,8 @@ class Human_Capital_Network():
 
     def run(self, rate_of_leave):
         l = len(self.position)
+        proformance = 0
+        unhealthy = 0
         tmp = [0 for i in range(l)]
         s = sum([1/self.position[i].PN for i in range(l)])
         for i in range(l):
@@ -115,6 +133,14 @@ class Human_Capital_Network():
                 self.invite(i)
         for i in range(l):
             if self.position[i] != None and self.position[i].EXP >= 0:
-                self.proformance_of_department[self.position[i].name_of_department] += self.proformance_of_position[i] #####wait
+                self.proformance_of_department[self.position[i].name_of_department] += (self.position[i].CA + self.position[i].EXP)/2 + self.position[i].NP * (self.position[i].level/7) #####wait
         ##cal team proformance
+        for i in range(self.proformance_of_department):
+            proformance_of_company += self.proformance_of_department[i]
+        for i in range(l):
+            if self.position[i].CA < ct.CA[self.level_of_position[i]]:
+                unhealthy += ct.CA[self.level_of_position[i]] - self.position[i].CA
+            if self.position[i].PN < ct.PN[self.level_of_position[i]]:
+                unhealthy += ct.PN[self.level_of_position[i]] - self.position[i].PN
         ##cal company proformance
+        return [unhealthy, proformance_of_company]
